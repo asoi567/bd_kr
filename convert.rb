1a661ac297cd54b6699acbe3bd48f73864afa507
Mikhail Dieterle <mikdiet@gmail.com>
2015-08-08 20:07:56 +0300

    filename converter

--- convert.rb
+++ convert.rb
@@ -1,9 +1,33 @@
 require 'fileutils'
 
-print 'Enter FIO: '
-fio = gets.strip[0, 3].upcase
+class Converter
+  attr_accessor :fio, :dir
 
-puts "Starting converting ASB -> #{fio}"
-dir = File.join('converted/', fio)
-FileUtils.rm_r dir if Dir.exists? dir
-Dir.mkdir dir
+  def initialize(fio)
+    @fio = fio.strip[0, 3].upcase
+    @dir = File.join('converted/', @fio)
+  end
+
+  def perform
+    FileUtils.rm_r dir if Dir.exists? dir
+    Dir.mkdir dir
+
+    Dir.glob('ASB/*').each do |file_name|
+      new_file_name = convert file_name.gsub(/^ASB\//, '')
+    end
+  end
+
+  private
+
+  def convert(string)
+    string
+      .gsub(/ASBF_/, "#{fio}F_")
+      .gsub(/asbf_/, "#{fio.downcase}f_")
+  end
+end
+
+print 'Enter FIO: '
+converter = Converter.new(gets)
+puts "Starting converting ASB -> #{converter.fio}"
+converter.perform
+puts "Done"
